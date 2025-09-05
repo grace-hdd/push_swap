27a08b186eb9095251e9e3ef915e95bc56320df6
grace <gracehadda@gmail.com>
2025-09-05 12:50:30 +0300

    Move sort_five function from sort_small.c to sort_five.c for proper separation

--- src/algorithm/sort_five.c
+++ src/algorithm/sort_five.c
@@ -1,57 +1,27 @@
 #include "../../includes/push_swap.h"
 
-static int find_min(t_stack *stack)
-{
-    t_node *current;
-    int min;
-    int min_pos;
-    int pos;
-
-    current = stack->top;
-    min = current->value;
-    min_pos = 0;
-    pos = 0;
-    while (current)
-    {
-        if (current->value < min)
-        {
-            min = current->value;
-            min_pos = pos;
-        }
-        current = current->next;
-        pos++;
-    }
-    return (min_pos);
-}
-
-static void push_min_to_b(t_stack *stack_a, t_stack *stack_b)
-{
-    int min_pos;
-    int size;
-
-    min_pos = find_min(stack_a);
-    size = stack_a->size;
-    if (min_pos <= size / 2)
-    {
-        while (min_pos-- > 0)
-            ra(stack_a);
-    }
-    else
-    {
-        while (min_pos++ < size)
-            rra(stack_a);
-    }
-    pb(stack_a, stack_b);
-}
-
 void sort_five(t_stack *stack_a, t_stack *stack_b)
 {
+    int min;
+    int i;
+
     if (is_sorted(stack_a))
         return;
-    push_min_to_b(stack_a, stack_b);
-    if (stack_a->size > 3)
-        push_min_to_b(stack_a, stack_b);
+    i = 0;
+    while (i < 2)
+    {
+        min = find_min(stack_a);
+        while (stack_a->top->value != min)
+        {
+            if (get_position(stack_a, min) <= stack_a->size / 2)
+                ra(stack_a);
+            else
+                rra(stack_a);
+        }
+        pb(stack_a, stack_b);
+        i++;
+    }
     sort_three(stack_a);
-    while (stack_b->size > 0)
-        pa(stack_a, stack_b);
+    pa(stack_a, stack_b);
+    pa(stack_a, stack_b);
 }
--- src/algorithm/sort_small.c
+++ src/algorithm/sort_small.c
@@ -45,29 +45,3 @@ void	sort_three(t_stack *stack)
 	}
 }
 
-void	sort_five(t_stack *stack_a, t_stack *stack_b)
-{
-	int	min;
-	int	max;
-	int	i;
-
-	if (is_sorted(stack_a))
-		return ;
-	i = 0;
-	while (i < 2)
-	{
-		min = find_min(stack_a);
-		while (stack_a->top->value != min)
-		{
-			if (get_position(stack_a, min) <= stack_a->size / 2)
-				ra(stack_a);
-			else
-				rra(stack_a);
-		}
-		pb(stack_a, stack_b);
-		i++;
-	}
-	sort_three(stack_a);
-	pa(stack_a, stack_b);
-	pa(stack_a, stack_b);
-}
