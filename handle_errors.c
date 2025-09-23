7116f0084eb2545467fc184861a02a18804ccdf2
Grace Haddad <grhaddad@c2r1s1.42beirut.com>
2025-09-23 15:34:37 +0300

    pdate

---
+++ handle_errors.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_errors.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grhaddad <grhaddad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/12 11:59:50 by grhaddad          #+#    #+#             */
+/*   Updated: 2025/09/12 12:54:44 by grhaddad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	error_syntax(char *str)
+{
+	if (!(*str == '+' || *str == '-' || (*str >= '0' && *str <= '9')))
+		return (1);
+	if ((*str == '+' || *str == '-') && !(*str >= '0' && *str <= '9'))
+		return (1);
+	while (*++str)
+	{
+		if (!(*str >= '0' && *str <= '9'))
+			return (1);
+	}
+	return (0);
+}
+
+int	error_duplicates(t_stack *a, int n)
+{
+	if (!a)
+		return (0);
+	while (a)
+	{
+		if (a->nbr == n)
+			return (1);
+		a = a->next;
+	}
+	return (0);
+}
+
+void	free_stack(t_stack **stack)
+{
+	t_stack	*tmp;
+	t_stack	*current;
+
+	if (!stack)
+		return ;
+	current = *stack;
+	while (current)
+	{
+		tmp = current->next;
+		current->nbr = 0;
+		free(current);
+		current = tmp;
+	}
+	*stack = NULL;
+}
+
+void	free_errors(t_stack **a)
+{
+	free_stack(a);
+	ft_printf("Error\n");
+	exit(1);
+}
